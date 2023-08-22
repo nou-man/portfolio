cccacd9f1287e6eb4299aaf00437f5633cb327a4
glug pace <4pa20cs059@pace.edu.in>
2023-08-22 16:40:59 +0530

    init:commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Mohammed Nouman</span> I am a Software Developer, currently pursuing bachelors degree in Computer Science and Engineering. I have gained hands-on experience on various projects ranging from Web Development, IoT, Cyber Security, Raspberry Pi and other development boards. I am passionate about using technology to solve real-world problems and always look for opportunities to learn and grow as an Engineer.  <br><br> I am indulged in lot of activities, doing workshops and sessions for other students and peers. Currently I am the President of GLUG PACE a club for tech geeks at our college "
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
